7715db2cf9ddbbf3cb69d443851eb90f62e9df5b
Michał Korlatowicz <korlatowicz44@gmail.com>
2023-10-23 19:38:17 +0200

    Commit on Visual Studio

--- Domain/JobQueueMVC.Domain/Model/FEAEngineer.cs
+++ Domain/JobQueueMVC.Domain/Model/FEAEngineer.cs
@@ -1,5 +1,8 @@
 public class FEAEnginner
 {
     public int Id {get;set;}
-    public string Name {get;set;}
+    public string FirstName {get;set;}
+    public string LastName { get;set;}
+    public string Position { get;set;}
+
 }
